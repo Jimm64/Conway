2867dc03fec71eb67a91ba1766cb5bf13dba3c44
Jim Carroll <jccarrll@gmail.com>
2021-06-28 21:01:46 -0500

    Use vertx arrays for faster drawing
    Add updateCount and control glut's mainloop ourselves so we can profile
    running time.

--- Screen.py
+++ Screen.py
@@ -1,15 +1,22 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import numpy
 
 import BoardState
 
 class GlScreen:
 
+
     def loop(self):
-        glutMainLoop()
+        while True:
+            glutMainLoopEvent()
+            self.boardState.update()
+            self.drawScreen()
+            self.updateCount += 1
 
     def __init__(self, boardState, width, height):
+        self.updateCount = 0
         self.displayWidth = width
         self.displayHeight = height
         self.boardState = boardState
@@ -32,26 +39,39 @@ class GlScreen:
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
+        glEnableClientState(GL_VERTEX_ARRAY)
+        self.corners = numpy.zeros(8 * self.boardState.cols * self.boardState.rows, dtype=numpy.float32)
+        glVertexPointer(2, GL_FLOAT, 0, self.corners)
+
     def drawScreen(self):
-        self.boardState.update()
 
         width = self.displayWidth / self.boardState.cols
         height = self.displayHeight / self.boardState.rows
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT)
 
         glColor3f(0, 0, 1)
-        glBegin(GL_QUADS)
+
+        activeCells = 0
         for row in range(0, self.boardState.rows):
             for col in range(0, self.boardState.cols):
                 if self.boardState.cellState(row, col):
                     x = row * width
                     y = col * height
-                    glVertex2f(x, y)
-                    glVertex2f(x, y + height)
-                    glVertex2f(x + width, y + height)
-                    glVertex2f(x + width, y)
-        glEnd()
+                    cell = activeCells * 8
+
+                    self.corners[cell + 0] = x
+                    self.corners[cell + 1] = y
+                    self.corners[cell + 2] = x
+                    self.corners[cell + 3] = y + height
+                    self.corners[cell + 4] = x + width
+                    self.corners[cell + 5] = y + height
+                    self.corners[cell + 6] = x + width
+                    self.corners[cell + 7] = y
+
+                    activeCells += 1
+
+        glDrawArrays(GL_QUADS, 0, activeCells * 4)
 
         glutSwapBuffers()
 
