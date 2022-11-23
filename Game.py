ae8c804ae51553d0ec16de52852bbde5697b0f9f
Jim Carroll <jccarrll@gmail.com>
2022-11-23 15:49:55 +0000

    Use argument parser for commandline options

--- Game.py
+++ Game.py
@@ -1,11 +1,18 @@
+import argparse
 import BoardState
 import Screen
 import sys
 
-boardState = BoardState.BoardState(int(sys.argv[1]), int(sys.argv[2]))
+arg_parser = argparse.ArgumentParser("Conway's Game of Life")
+
+arg_parser.add_argument("--cell-dimensions", dest="cell_dimensions", nargs=2, required=False, type=int, default=[100,100], help="Number of cell columns and rows")
+arg_parser.add_argument("--screen-dimensions", dest="screen_dimensions", nargs=2, required=False, type=int, default=[400,400], help="Screen width and height")
+args = arg_parser.parse_args()
+
+boardState = BoardState.BoardState(*args.cell_dimensions)
 boardState.randomizeState()
 
-screen = Screen.GlScreen(boardState, int(sys.argv[3]), int(sys.argv[4]))
+screen = Screen.GlScreen(boardState, *args.screen_dimensions)
 
 screen.loop()
 
